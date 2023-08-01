0cef21214b47112acd1e41804e106b1ed3560039
lishanisrikaran <l.srikaran@outlook.com>
2023-08-01 20:53:14 +0100

    Adding data folder

--- static/js/logic.js
+++ static/js/logic.js
@@ -60,4 +60,4 @@ function createMarkers(response) {
 }
 
 // Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
-d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json").then(createMarkers);
+d3.json("data/data.json").then(createMarkers);
